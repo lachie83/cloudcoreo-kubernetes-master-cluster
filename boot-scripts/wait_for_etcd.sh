896918b3a0fbf8f158fbf17fea9e58f19b011635
Paul Allen <pauldallen@gmail.com>
2016-07-26 14:54:49 -0700

    add a flag to wait for etcd

--- boot-scripts/wait_for_etcd.sh
+++ boot-scripts/wait_for_etcd.sh
@@ -5,9 +5,11 @@ dns_zone="${ETCD_DNS_ZONE}"
 
 (
     cd /opt/etcd
-    while ./etcdctl --endpoint "http://${cluster_name}.${dns_zone}:2379" cluster-health 2>&1 | grep -q -e "exceeded header timeout" -e "failed to list members" -e "etcd cluster is unavailable or misconfigured"; do
-	echo "waiting for etcd";
-	sleep 5
-    done
+    if [ "${WAIT_FOR_ETCD:-true}" = "true" ]; then
+	while ./etcdctl --endpoint "http://${cluster_name}.${dns_zone}:2379" cluster-health 2>&1 | grep -q -e "exceeded header timeout" -e "failed to list members" -e "etcd cluster is unavailable or misconfigured"; do
+	    echo "waiting for etcd";
+	    sleep 5
+	done
+    fi
 )
 
